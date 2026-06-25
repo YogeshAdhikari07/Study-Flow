e4359fcc45f78fa0aa9da4373914c3ef8b99fcb9
Yogesh Adhikari <124176353+YogeshAdhikari07@users.noreply.github.com>
2026-06-25 07:19:05 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ server.set('view engine','ejs');
 server.get('/', (req, res) => {
     res.send('hello');
 });
+server.get("/login",(req,res)=>{
+    res.send('Login')
+})
 server.listen(5000, () => {
     console.log('Server Started...');
 });
