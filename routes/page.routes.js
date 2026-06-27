c0aef8f82d6dd2a0da33cc33648d1694faffb397
Yogesh Adhikari <adhikariprema29@gmail.com>
2026-06-27 06:12:01 +0530

    New Pages Updated!

--- routes/page.routes.js
+++ routes/page.routes.js
@@ -3,4 +3,7 @@ const page = express.Router();
 page.get('/leaderboard',(req,res)=>{
     res.render('leaderboard');
 });
+page.get('/logsign',(req,res)=>{
+    res.render('logsign');
+})
 module.exports = page;
