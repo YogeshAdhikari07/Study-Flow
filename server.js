01dd5e949cd873d957ea8e9d1df05194211c3fa8
Yogesh Adhikari <adhikariprema29@gmail.com>
2026-07-12 19:22:37 +0530

    Updated New UI

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
+server.use(cors());
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config();
